c7f7a2de31fe4f7a133b52a596de0b772a384329
James Robb <james@jamesrobb.co.uk>
2020-12-04 02:10:20 +0100

    Optimising solution

--- 01 - Report Repair/index.js
+++ 01 - Report Repair/index.js
@@ -1,33 +1,38 @@
 const { promises } = require("fs");
 const { join } = require("path");
 
+function first(expenses) {
+  const checked = new Set();
+
+  for (const expense of expenses) {
+    const target = 2020 - expense;
+    if (checked.has(target)) return target * expense;
+    checked.add(expense);
+  }
+
+  return null;
+}
+
+function second(expenses) {
+  const targets = new Map();
+
+  for (const outer of expenses) {
+    const target = 2020 - outer;
+    if (targets.has(target)) return outer * targets.get(target);
+    for (const inner of expenses) targets.set(outer + inner, outer * inner);
+  }
+
+  return null;
+}
+
 async function main() {
   const uri = join(__dirname, "input.txt");
   const file = await promises.readFile(uri, "utf8");
   const expenses = file.split("\n").map(item => parseInt(item, 10));
-  const part_one = new Array(2);
-  const part_two = new Array(3);
-
-  for (const outer of expenses) {
-    for (const middle of expenses) {
-      for (const inner of expenses) {
-        if (middle + inner === 2020) {
-          part_one[0] = middle;
-          part_one[1] = inner;
-        }
-
-        if (outer + middle + inner === 2020) {
-          part_two[0] = outer;
-          part_two[1] = middle;
-          part_two[2] = inner;
-        }
-      }
-    }
-  }
 
   console.log({
-    part_one: part_one.reduce((a, c) => a * c, 1),
-    part_two: part_two.reduce((a, c) => a * c, 1)
+    part_one: first(expenses),
+    part_two: second(expenses)
   });
 }
 
