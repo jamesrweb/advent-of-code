b30166a70738dc4b8e256aaa4e0432365246ac73
James Robb <james@jamesrobb.co.uk>
2020-12-12 17:19:52 +0100

    Tidying solution

--- 11 - Seating System/index.rs
+++ 11 - Seating System/index.rs
@@ -2,11 +2,7 @@
 use std::fs::{read_to_string};
 
 #[derive(PartialEq, Clone, Copy)]
-enum CellState {
-    Floor,
-    Empty,
-    Occupied
-}
+enum CellState { Floor, Empty, Occupied }
 
 type Coordinates = (i32, i32);
 type CellStates = Vec<CellState>;
@@ -23,18 +19,18 @@ const NEIGHBOURS: [Coordinates; 8] = [
 ];
 
 fn read_lines(filename: &str) -> String {
-    read_to_string(filename).unwrap()
+    return read_to_string(filename).unwrap();
 }
 
-fn generate_grid(lines: String) -> Grid {
-    lines.lines().map(to_states).collect()
+fn generate_grid(data: String) -> Grid {
+    return data.lines().map(to_cell_states).collect();
 }
 
-fn to_states(line: &str) -> CellStates {
-    line.chars().map(to_state).collect::<CellStates>()
+fn to_cell_states(line: &str) -> CellStates {
+    return line.chars().map(to_cell_state).collect::<CellStates>();
 }
 
-fn to_state(character: char) -> CellState {
+fn to_cell_state(character: char) -> CellState {
     return match character {
         '#' => CellState::Occupied,
         'L' => CellState::Empty,
@@ -47,35 +43,39 @@ fn in_range(grid: &Grid, x: i32, y: i32) -> bool {
     let width = grid[0].len() as i32;
     let horizontal = x >= 0 && x < width;
     let vertical = y >= 0 && y < height;
-
-    horizontal && vertical
+    return horizontal && vertical;
 }
 
 fn cell_state(grid: &Grid, x: i32, y: i32) -> CellState {
-    grid[y as usize][x as usize]
+    return grid[y as usize][x as usize];
 }
 
-fn compare_states(first: CellState, second: CellState) -> bool {
-    first == second
+fn states_match(first: CellState, second: CellState) -> bool {
+    return first == second;
 }
 
 fn next_step(x: i32, y: i32, dx: i32, dy: i32) -> Coordinates {
-    (x + dx, y + dy)
+    return (x + dx, y + dy);
 }
 
+fn on_floor(grid: &Grid, nx: i32, ny: i32) -> bool {
+    if !in_range(&grid, nx, ny) { return false; }
+    let state = cell_state(&grid, nx, ny);
+    return states_match(state, CellState::Floor);
+}
 
 fn poc(grid: &Grid, x: i32, y: i32) -> i32 {
     let mut count = 0;
 
     for &(dx, dy) in NEIGHBOURS.iter() {
         let (nx, ny) = next_step(x, y, dx, dy);
-        if in_range(&grid, nx, ny) && compare_states(
-            cell_state(&grid, nx, ny),
-            CellState::Occupied
-        ) { count += 1; }
+        if !in_range(&grid, nx, ny) { continue }
+        let state = cell_state(&grid, nx, ny);
+        if !states_match(state, CellState::Occupied) { continue }
+        count += 1;
     }
 
-    count
+    return count;
 }
 
 fn ptc(grid: &Grid, x: i32, y: i32) -> i32 {
@@ -84,25 +84,22 @@ fn ptc(grid: &Grid, x: i32, y: i32) -> i32 {
     for &(dx, dy) in NEIGHBOURS.iter() {
         let (mut nx, mut ny) = next_step(x, y, dx, dy);
 
-        while in_range(&grid, nx, ny) && compare_states(
-            cell_state(&grid, nx, ny),
-            CellState::Floor
-        ) {
+        while on_floor(&grid, nx, ny) {
             let (x, y) = next_step(nx, ny, dx, dy);
             nx = x;
             ny = y;
         }
 
-        if in_range(&grid, nx, ny) && compare_states(
-            cell_state(&grid, nx, ny),
-            CellState::Occupied
-        ) { count += 1; }
+        if !in_range(&grid, nx, ny) { continue }
+        let state = cell_state(&grid, nx, ny);
+        if !states_match(state, CellState::Occupied) { continue }
+        count += 1;
     }
 
-    count
+    return count;
 }
 
-fn run<C>(
+fn calulate_next_state<C>(
     grid: Grid,
     tolerance: i32,
     counter: C
@@ -111,25 +108,22 @@ fn run<C>(
 
     for y in 0..grid.len() {
         for x in 0..grid[0].len() {
-            if compare_states(
-                cell_state(&grid, x as i32, y as i32),
-                CellState::Floor
-            ) { continue }
-
-            let occupied = compare_states(
-                cell_state(&grid, x as i32, y as i32),
-                CellState::Occupied
-            );
+            let state = cell_state(&grid, x as i32, y as i32);
+            if states_match(state, CellState::Floor) { continue }
+            let occupied = states_match(state, CellState::Occupied);
             let neighbour_count = counter(&grid, x as i32, y as i32);
 
-            next_state[y][x] =
-                if occupied && neighbour_count >= tolerance { CellState::Empty }
-                else if !occupied && neighbour_count == 0 { CellState::Occupied }
-                else { grid[y][x] }
+            next_state[y][x] = if occupied && neighbour_count >= tolerance {
+                CellState::Empty
+            } else if !occupied && neighbour_count == 0 {
+                CellState::Occupied
+            } else {
+                grid[y][x]
+            }
         }
     }
 
-    next_state
+    return next_state;
 }
 
 fn run_simulation<G, C: 'static>(
@@ -139,21 +133,23 @@ fn run_simulation<G, C: 'static>(
     tolerance: i32
 ) -> usize where G: StateGenerator, C: Counter + Copy {
     let mut previous_state = grid.clone();
-    let mut next_state = generate(grid, tolerance, Box::new(counter));
+    let mut current_state = generate(grid, tolerance, Box::new(counter));
 
-    while previous_state != next_state {
-        previous_state = next_state.clone();
-        next_state = generate(next_state, tolerance, Box::new(counter));
+    while previous_state != current_state {
+        previous_state = current_state.clone();
+        current_state = generate(current_state, tolerance, Box::new(counter));
     }
 
-    next_state.iter().flatten().filter(|&&cell| {
-        compare_states(cell, CellState::Occupied)
-    }).count()
+    return current_state.iter().flatten().filter(|&&cell| {
+        states_match(cell, CellState::Occupied)
+    }).count();
 }
 
 fn main() {
     let lines = read_lines("./input.txt");
     let grid = generate_grid(lines);
-    println!("part_one: {}", run_simulation(run, poc, grid.clone(), 4));
-    println!("part_two: {}", run_simulation(run, ptc, grid.clone(), 5));
+    let part_one = run_simulation(calulate_next_state, poc, grid.clone(), 4);
+    let part_two = run_simulation(calulate_next_state, ptc, grid.clone(), 5);
+    println!("part_one: {}", part_one);
+    println!("part_two: {}", part_two);
 }
