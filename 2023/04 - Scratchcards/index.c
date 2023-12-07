effeac951ee8c1adc2320d0147987f6f0273a263
jamesrweb <47126579+jamesrweb@users.noreply.github.com>
2023-12-07 21:00:44 +0100

    2023 day 4 part 2

--- 2023/04 - Scratchcards/index.c
+++ 2023/04 - Scratchcards/index.c
@@ -2,96 +2,134 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Game
+#define MAX_LINE_LENGTH 256
+#define INITIAL_ARRAY_SIZE 10
+
+struct Card
 {
-    int gameId;
+    int cardId;
     int *winningNumbers;
     int winningNumbersCount;
     int *myNumbers;
     int myNumbersCount;
 };
 
-void freeGame(struct Game *game)
+void freeCard(struct Card *card)
 {
-    free(game->winningNumbers);
-    free(game->myNumbers);
+    free(card->winningNumbers);
+    free(card->myNumbers);
 }
 
-struct Game toGame(const char *line)
+struct Card toCard(const char *line)
 {
-    struct Game game;
-    char *winningNumbersStr = strtok((char *)line, "|");
+    struct Card card;
+    char *winningNumbersStr = strtok(strdup(line), "|");
     char *myNumbersStr = strtok(NULL, "|");
     char *token = strtok(winningNumbersStr + 8, " ");
 
-    sscanf(line, "Card %d:", &game.gameId);
-    game.winningNumbers = (int *)malloc(sizeof(int));
-    game.winningNumbersCount = 0;
+    sscanf(line, "Card %d:", &card.cardId);
+    card.winningNumbers = (int *)malloc(INITIAL_ARRAY_SIZE * sizeof(int));
+    card.winningNumbersCount = 0;
 
     while (token != NULL)
     {
-        game.winningNumbers[game.winningNumbersCount++] = atoi(token);
-        game.winningNumbers = (int *)realloc(game.winningNumbers, (game.winningNumbersCount + 1) * sizeof(int));
+        if (card.winningNumbersCount >= INITIAL_ARRAY_SIZE)
+        {
+
+            card.winningNumbers = (int *)realloc(card.winningNumbers, 2 * card.winningNumbersCount * sizeof(int));
+        }
+
+        card.winningNumbers[card.winningNumbersCount++] = atoi(token);
         token = strtok(NULL, " ");
     }
 
-    game.myNumbers = (int *)malloc(sizeof(int));
-    game.myNumbersCount = 0;
+    card.myNumbers = (int *)malloc(INITIAL_ARRAY_SIZE * sizeof(int));
+    card.myNumbersCount = 0;
     token = strtok(myNumbersStr, " ");
 
     while (token != NULL)
     {
-        game.myNumbers[game.myNumbersCount++] = atoi(token);
-        game.myNumbers = (int *)realloc(game.myNumbers, (game.myNumbersCount + 1) * sizeof(int));
+        if (card.myNumbersCount >= INITIAL_ARRAY_SIZE)
+        {
+
+            card.myNumbers = (int *)realloc(card.myNumbers, 2 * card.myNumbersCount * sizeof(int));
+        }
+
+        card.myNumbers[card.myNumbersCount++] = atoi(token);
         token = strtok(NULL, " ");
     }
 
-    return game;
+    free(winningNumbersStr);
+    return card;
 }
 
-struct Game *parseGames(FILE *file, int *gameCount)
+struct Card *parseCards(FILE *file, int *cardCount)
 {
-    char line[256];
-    struct Game *games = NULL;
+    char line[MAX_LINE_LENGTH];
+    struct Card *cards = NULL;
 
-    *gameCount = 0;
+    *cardCount = 0;
 
     while (fgets(line, sizeof(line), file) != NULL)
     {
         line[strcspn(line, "\n")] = '\0';
-        struct Game game = toGame(line);
-        games = (struct Game *)realloc(games, (*gameCount + 1) * sizeof(struct Game));
-        games[(*gameCount)++] = game;
+        struct Card card = toCard(line);
+        cards = (struct Card *)realloc(cards, (*cardCount + 1) * sizeof(struct Card));
+        cards[(*cardCount)++] = card;
     }
 
-    return games;
+    return cards;
 }
 
-int countMatchingNumbers(const int *winningNumbers, int winningNumbersSize, const int *myNumbers, int myNumbersSize)
+struct Card cloneCard(const struct Card *original)
 {
-    int count = 0;
+    struct Card clone;
 
-    for (int i = 0; i < winningNumbersSize; i++)
+    clone.cardId = original->cardId;
+    clone.winningNumbersCount = original->winningNumbersCount;
+    clone.myNumbersCount = original->myNumbersCount;
+
+    clone.winningNumbers = original->winningNumbers;
+    clone.myNumbers = original->myNumbers;
+
+    return clone;
+}
+
+struct MatchingNumbersResult
+{
+    int *matches;
+    int size;
+};
+
+struct MatchingNumbersResult findMatchingNumbersForCard(struct Card card)
+{
+    struct MatchingNumbersResult result;
+    result.matches = NULL;
+    result.size = 0;
+
+    for (int i = 0; i < card.winningNumbersCount; i++)
     {
-        for (int j = 0; j < myNumbersSize; j++)
+        for (int j = 0; j < card.myNumbersCount; j++)
         {
-            if (myNumbers[j] == winningNumbers[i])
+            if (card.myNumbers[j] == card.winningNumbers[i])
             {
-                count++;
+                result.size++;
+                result.matches = (int *)realloc(result.matches, result.size * sizeof(int));
+                result.matches[result.size - 1] = card.myNumbers[j];
                 break;
             }
         }
     }
 
-    return count;
+    return result;
 }
 
-int scoreForGame(struct Game game)
+int scoreForCard(struct Card card)
 {
-    int matchingNumbers = countMatchingNumbers(game.winningNumbers, game.winningNumbersCount, game.myNumbers, game.myNumbersCount);
+    struct MatchingNumbersResult matchingNumbers = findMatchingNumbersForCard(card);
     int score = 0;
 
-    for (int i = 0; i < matchingNumbers; i++)
+    for (int i = 0; i < matchingNumbers.size; i++)
     {
         if (score == 0)
         {
@@ -105,22 +143,48 @@ int scoreForGame(struct Game game)
     return score;
 }
 
-int scoreAcrossGames(struct Game *games, int *gameCount)
+int scoreAcrossCards(struct Card *cards, int *cardCount)
 {
     int score = 0;
 
-    for (int i = 0; i < *gameCount; i++)
+    for (int i = 0; i < *cardCount; i++)
     {
-        struct Game game = games[i];
-        score += scoreForGame(game);
-    }
-
-    for (int i = 0; i < *gameCount; i++)
-    {
-        freeGame(&games[i]);
+        struct Card card = cards[i];
+        score += scoreForCard(card);
     }
 
     return score;
+}
+
+int scratchCardsWon(struct Card *cards, int *cardCount)
+{
+    int wonCards[*cardCount];
+    int matchCount = 0;
+
+    for (int i = 0; i < *cardCount; i++)
+    {
+        wonCards[i] = 1;
+    }
+
+    for (int i = 0; i < *cardCount; i++)
+    {
+        struct MatchingNumbersResult matches = findMatchingNumbersForCard(cards[i]);
+        matchCount = matches.size;
+
+        for (int j = 1; j < matchCount + 1; j++)
+        {
+            wonCards[i + j] += 1 * wonCards[i];
+        }
+    }
+
+    int sum = 0;
+
+    for (int i = 0; i < sizeof(wonCards) / sizeof(wonCards[0]); i++)
+    {
+        sum += wonCards[i];
+    }
+
+    return sum;
 }
 
 int main()
@@ -135,12 +199,13 @@ int main()
         return 1;
     }
 
-    int gameCount;
-    struct Game *games = parseGames(file, &gameCount);
-    int totalScore = scoreAcrossGames(games, &gameCount);
+    int cardCount;
+    struct Card *cards = parseCards(file, &cardCount);
 
-    printf("Part one: %d", totalScore);
-    free(games);
+    printf("Part one: %d\n", scoreAcrossCards(cards, &cardCount));
+    printf("Part two: %d\n", scratchCardsWon(cards, &cardCount));
+
+    free(cards);
     fclose(file);
 
     return 0;
