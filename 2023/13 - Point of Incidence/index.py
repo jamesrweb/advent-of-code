0f8f58ad37b81c63a2c276da0d4f53379e202242
jamesrweb <47126579+jamesrweb@users.noreply.github.com>
2023-12-18 01:47:13 +0100

    2023 day 13 refactor

--- 2023/13 - Point of Incidence/index.py
+++ 2023/13 - Point of Incidence/index.py
@@ -12,24 +12,7 @@ def to_binary(text: Text) -> int:
     return sum(2**index if column == "#" else 0 for index, column in enumerate(text))
 
 
-def find_mirror_part_one(binaries: List[int]) -> int:
-    for index in range(1, len(binaries)):
-        outer_index = index
-        inner_index = index - 1
-
-        while outer_index < len(binaries) and inner_index >= 0:
-            if binaries[outer_index] != binaries[inner_index]:
-                break
-
-            outer_index += 1
-            inner_index -= 1
-        else:
-            return index
-
-    return 0
-
-
-def find_mirror_part_two(binaries: List[int]) -> int:
+def find_mirror(binaries: List[int], is_part_one: bool) -> int:
     for index in range(1, len(binaries)):
         outer_index = index
         inner_index = index - 1
@@ -37,6 +20,9 @@ def find_mirror_part_two(binaries: List[int]) -> int:
 
         while outer_index < len(binaries) and inner_index >= 0:
             if binaries[outer_index] != binaries[inner_index]:
+                if is_part_one:
+                    break
+
                 if error:
                     break
 
@@ -50,34 +36,35 @@ def find_mirror_part_two(binaries: List[int]) -> int:
             outer_index += 1
             inner_index -= 1
         else:
-            if error:
+            if is_part_one or error:
                 return index
 
     return 0
 
 
-def solve_part_one(grids: Grids) -> int:
+def solve(grids: Grids, is_part_one: bool) -> int:
     mirrors = 0
 
     for grid in grids:
         rows = [to_binary(row) for row in grid]
         columns = [to_binary(column) for column in zip(*grid)]
+        row_mirrors = find_mirror(rows, is_part_one)
+        column_mirrors = find_mirror(columns, is_part_one)
 
-        mirrors += find_mirror_part_one(rows) * 100 + find_mirror_part_one(columns)
+        if is_part_one:
+            mirrors += 100 * row_mirrors + column_mirrors
+        else:
+            mirrors += 100 * row_mirrors or column_mirrors
 
     return mirrors
+
+
+def solve_part_one(grids: Grids) -> int:
+    return solve(grids, True)
 
 
 def solve_part_two(grids: Grids) -> int:
-    mirrors = 0
-
-    for grid in grids:
-        rows = [to_binary(row) for row in grid]
-        columns = [to_binary(column) for column in zip(*grid)]
-
-        mirrors += 100 * find_mirror_part_two(rows) or find_mirror_part_two(columns)
-
-    return mirrors
+    return solve(grids, False)
 
 
 with open("input.txt", "r") as file:
